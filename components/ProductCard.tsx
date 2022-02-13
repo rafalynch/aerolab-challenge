3627862e5890cffc64a598c7bfd7f69d3c8d2240
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-13 13:09:10 -0300

    Created product card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,81 @@
 import React from "react";
-import {} from "@chakra-ui/react";
+import { Box, Image, Text, Tag } from "@chakra-ui/react";
 
-import { GetServerSideProps, NextPage } from "next";
+import { Product } from "../types";
 
-export default function ProductCard<NextPage>(props: GetServerSideProps) {
-  console.log(props);
-  return <div>Hola</div>;
+function ProductCard(product: Product) {
+  return (
+    <Box
+      maxHeight={"300px"}
+      maxWidth="350px"
+      position={"relative"}
+      role="group"
+      cursor={"pointer"}
+    >
+      <Box
+        display={"block"}
+        position="absolute"
+        bg="aerolab.primary"
+        height="100%"
+        bottom={0}
+        opacity={0}
+        width={"100%"}
+        transition={"0.1s"}
+        _groupHover={{
+          opacity: 0.6,
+          height: "100%",
+          width: "100%",
+          boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
+        }}
+        color="white"
+      />
+
+      <Box
+        position={"absolute"}
+        display={"grid"}
+        textAlign={"center"}
+        height="100%"
+        width="100%"
+        justifyContent={"center"}
+        alignContent="center"
+        color="white"
+        opacity={0}
+        transition={"0.2s"}
+        _groupHover={{
+          opacity: 1,
+        }}
+      >
+        <Text fontSize={"4xl"} margin={0} p={0}>
+          {product.cost}
+        </Text>
+        <Tag borderRadius="full" px="25px" py="5px" bg={"white"}>
+          Reedem now
+        </Tag>
+      </Box>
+
+      <Box
+        padding={"25px"}
+        maxW="300px"
+        backgroundColor={"white"}
+        boxShadow={"rgb(38, 57, 77) 0px 3px 20px -5px"}
+      >
+        <Image src={product.img.url} backgroundColor="rgb(0,0,0,0)" w="200px" />
+        <Box>
+          <Box>{product.category}</Box>
+          <Box
+            my="3px"
+            mx="0"
+            fontWeight="semibold"
+            as="h3"
+            lineHeight="tight"
+            isTruncated
+          >
+            {product.name}
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
+
+export default ProductCard;
